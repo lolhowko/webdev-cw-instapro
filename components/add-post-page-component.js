e8277961439526cc405d2b87dc81927779912808
Lana <lolhowko@gmail.com>
2023-09-22 01:13:59 +0300

    add header to add-posts

--- components/add-post-page-component.js
+++ components/add-post-page-component.js
@@ -1,3 +1,5 @@
+import { renderHeaderComponent } from "./header-component.js";
+
 export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
   const render = () => {
     // TODO: Реализовать страницу добавления поста
@@ -33,6 +35,10 @@ export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
   `;
 
     appEl.innerHTML = appHtml;
+
+    renderHeaderComponent({
+      element: document.querySelector(".header-container"),
+    });
 
     document.getElementById("add-button").addEventListener("click", () => {
       onAddPostClick({
